fc551727a448108cf286e1131d783d39fdfa5d78
Jimin <bokjimin143@gmail.com>
2025-10-26 06:14:16 +0900

    feat: 예외 메시지를 상수로 관리하는 ErrorMessage 클래스 추가
    - 자동차 이름, 시도 횟수 관련 에러 메시지를 상수로 분리

---
+++ src/main/java/racingcar/constant/ErrorMessage.java
@@ -0,0 +1,15 @@
+package racingcar.constant;
+
+public class ErrorMessage {
+
+    public static final String ERROR_CAR_NAME_EMPTY = "자동차 이름은 비어 있을 수 없습니다.";
+    public static final String ERROR_CAR_NAME_TOO_LONG = "자동차 이름은 5자 이하만 가능합니다.";
+    public static final String ERROR_CAR_NAME_BLANK = "자동차 이름은 빈 값을 입력할 수 없습니다.";
+
+    public static final String ERROR_CAR_COUNT_TOO_SMALL = "경주에 참여할 자동차는 최소 2대 이상이어야 합니다.";
+    public static final String ERROR_CAR_NAME_DUPLICATE = "자동차 이름은 중복될 수 없습니다.";
+
+    public static final String ERROR_ROUND_NOT_POSITIVE = "시도할 횟수를 숫자로 입력해주세요.";
+    public static final String ERROR_ROUND_NOT_NUMBER = "시도할 횟수를 양수로 입력해주세요.";
+
+}
