18c44e336cba75772abfb001ffcdac4f20a4778e
Jimin <bokjimin143@gmail.com>
2025-10-26 04:35:56 +0900

    feat(Input): 자동차 이름 입력 기능 추가
    - Input에 자동차 이름 입력 기능 추가
    - Console 입력 후 쉼표(,) 기준으로 이름 분리

---
+++ src/main/java/racingcar/view/Input.java
@@ -0,0 +1,19 @@
+package racingcar.view;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.Arrays;
+import java.util.List;
+
+import static racingcar.constant.InputMessage.*;
+
+public class Input {
+
+    public List<String> inputCarNames() {
+        System.out.println(CAR_NAMES_INPUT_MESSAGE);
+        String CarNames = Console.readLine();
+        return Arrays.stream(CarNames.split(",")).toList();
+    }
+
+
+}
