c958d26a5dc47732fecd0661ff9d4a3a92a20c51
Jimin <bokjimin143@gmail.com>
2025-10-26 08:45:46 +0900

    feat(Output): 실행 결과 제목 출력 기능 추가
    - OutputMessage 클래스에 RESULT_TITLE 상수 정의
    - Output 클래스에서 printResultTitle() 메서드로 결과 제목 출력

---
+++ src/main/java/racingcar/constant/OutputMessage.java
@@ -0,0 +1,7 @@
+package racingcar.constant;
+
+public class OutputMessage {
+
+    public static final String RESULT_TITLE = "\n실행 결과";
+
+}
---
+++ src/main/java/racingcar/view/Output.java
@@ -0,0 +1,12 @@
+package racingcar.view;
+
+import static racingcar.constant.OutputMessage.*;
+
+public class Output {
+
+    public void printResultTitle() {
+        System.out.println(RESULT_TITLE);
+    }
+
+    
+}
