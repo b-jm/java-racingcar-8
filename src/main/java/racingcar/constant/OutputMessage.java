cfb2464be6d3bce001bacf90d425c2b9d663b3e5
Jimin <bokjimin143@gmail.com>
2025-10-27 23:39:58 +0900

    feat(OutputMessage): 우승자 출력용 상수 추가
    - 최종 우승자 출력 시 사용될 메시지 상수(WINNER_MESSAGE) 추가

--- src/main/java/racingcar/constant/OutputMessage.java
+++ src/main/java/racingcar/constant/OutputMessage.java
@@ -3,5 +3,6 @@ package racingcar.constant;
 public class OutputMessage {
 
     public static final String RESULT_TITLE = "\n실행 결과";
+    public static final String WINNER_MESSAGE = "최종 우승자 : ";
 
 }
