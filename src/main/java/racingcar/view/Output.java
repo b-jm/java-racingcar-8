55d520fbce0ace5c84d7d0f45aa76c2e72de16d0
Jimin <bokjimin143@gmail.com>
2025-10-27 23:46:09 +0900

    feat(Output): 경주 결과 및 우승자 출력 기능 구현
    - 자동차의 이동 상태를 출력하는 printCarsStatus() 메서드 추가
    - 최종 우승자 목록을 출력하는 printWinners() 메서드 구현
    - 실행 결과 제목을 출력하는 printResultTitle() 메서드 유지
    - DISTANCE_UNIT 상수("-") 추가로 출력 포맷 일관성 확보

--- src/main/java/racingcar/view/Output.java
+++ src/main/java/racingcar/view/Output.java
@@ -1,12 +1,32 @@
 package racingcar.view;
 
+import racingcar.domain.Car;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import static racingcar.constant.OutputMessage.*;
 
 public class Output {
+
+    public static final String DISTANCE_UNIT = "-";
 
     public void printResultTitle() {
         System.out.println(RESULT_TITLE);
     }
 
-    
+    public void printCarsStatus(List<Car> cars) {
+        for (Car car : cars) {
+            System.out.println(car.getName() + " : " + DISTANCE_UNIT.repeat(car.getPosition()));
+        }
+        System.out.println();
+    }
+
+    public void printWinners(List<Car> winners) {
+        String winnersName = winners.stream()
+                .map(Car::getName)
+                .collect(Collectors.joining(", "));
+        System.out.println(WINNER_MESSAGE + winnersName);
+    }
+
 }
